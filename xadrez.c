1b2c830912d18a3521f6a0e46e49e2b3cf985027
R0dek <114034361+R0dek@users.noreply.github.com>
2025-10-28 18:40:29 +0000

    Adição do código do nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -2,33 +2,73 @@
 
 int main()
 {
-    int t = 1, b, r = 1;  //Variáveis de cada peça
+    int t = 0, b = 0, r = 0;  //Variáveis de cada peça
+
+   
+    movitorre(t);
+    movirainha(r);
+    movibispo(b);
+    movicavalo();
+}
 
 
 //Implementação da movimentação da torre
-    while (t <= 5)
-    {
-        printf("A torre andou %d casas para a direita!\n", t);
-        t++;
-    }
-printf("----------------------------------------------------\n\n");
-
-
-//Implementação da movimentação do bispo
-for (b = 1; b <= 5; b++)
+void movitorre(int t)
 {
-    printf("O bispo andou %d casas para cima, direita!\n", b);
+    if (t <= 4)
+    {
+        printf("A torre moveu %d casas para a direita!\n", t+1);
+        movitorre(t+1);
+    }
 
 }
-printf("----------------------------------------------------\n\n");
+
+//Implementação da movimentação do bispo
+void movibispo(int b)
+{
+    if (b < 1)
+    {      
+        for (int i; i < 4; i++)
+        {
+            for (int j; j < 5; j++)
+            {
+                printf("O bispo moveu %d casas para cima!\n", j+1);
+            }
+            printf("O bispo moveu %d casas para a direita!\n", i+1);
+        }
+        movibispo(b+1);
+    }
+}
 
 
 //Implementação da movimentação da rainha
-    do
+void movirainha(int r)
+{
+    if (r < 8 )
     {
-        printf("A rainha andou %d casas para a esquerda!\n", r);
-        r++;
-    } while (r <= 8);
-
-printf("----------------------------------------------------\n\n");
+        printf("A Rainha moveu %d casas para a esquerda\n", r+1);
+        movirainha(r+1);
+    }
+    
 }
+
+
+//Implementação da movimentação do cavalo
+
+void movicavalo()
+{
+        for (int i = 0; i < 1; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                printf("O cavalo moveu %d casas para cima!\n", j+1);
+            }
+            printf("O cavalo moveu %d casa para a direita!\n", i+1);
+        }    
+}
+
+
+
+
+
+
